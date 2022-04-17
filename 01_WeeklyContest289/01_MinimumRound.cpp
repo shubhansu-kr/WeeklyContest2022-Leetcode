a5ad1928ae81bec2c0e1f1543ca719174b40c8a6
shubhansu-kr <shubhansu2021@gmail.com>
2022-04-17 09:10:47 +0530

    Wrong answer

--- 01_WeeklyContest289/01_MinimumRound.cpp
+++ 01_WeeklyContest289/01_MinimumRound.cpp
@@ -11,7 +11,55 @@
 using namespace std;
 
 class Solution
+{   
+    // Wrong answer 
+    // [66,66,63,61,63,63,64,66,66,65,66,65,61,67,68,66,62,67,61,64,66,60,69,66,65,68,63,60,
+    // 67,62,68,60,66,64,60,60,60,62,66,64,63,65,60,69,63,68,68,69,68,61]
+public:
+    int minimumRounds(vector<int> &tasks)
+    {
+        map<int, int> frequency;
+        for (int i = 0; i < tasks.size(); i++)
+        {
+            frequency[tasks[i]]++;
+        }
+        int round = 0;
+        for (auto it : frequency)
+        {
+            // it is a pair
+            if (it.second % 3 == 0)
+            {
+                round += (it.second / 3);
+                continue;
+            }
+            else
+            {
+                if ((it.second % 3) == 2)
+                {
+                    round += it.second / 3 + 1;
+                    continue;
+                }
+                else
+                {
+                    if (it.second % 2 == 0)
+                    {
+                        round += it.second / 2;
+                        continue;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+            }
+        }
+        return round;
+    }
+};
+
+class Solution
 {
+    // Wrong solution : order of evaluation wrong
 public:
     int minimumRounds(vector<int> &tasks)
     {
