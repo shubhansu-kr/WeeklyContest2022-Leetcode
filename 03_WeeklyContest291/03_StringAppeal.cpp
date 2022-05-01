72126d7d75b0c435cb5fc2452bf1661b7376e572
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-01 09:04:01 +0530

    Total Appeal

---
+++ 03_WeeklyContest291/03_StringAppeal.cpp
@@ -0,0 +1,26 @@
+// 6050. Total Appeal of A String
+
+// The appeal of a string is the number of distinct characters 
+// found in the string.
+
+// For example, the appeal of "abbca" is 3 because it has 3 
+// distinct characters: 'a', 'b', and 'c'.
+// Given a string s, return the total appeal of all of its substrings.
+
+// A substring is a contiguous sequence of characters 
+// within a string.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    long long appealSum(string s) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
