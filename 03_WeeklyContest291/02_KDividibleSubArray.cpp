699a39dedaeb43a79fc794e6ba69dfe2ab7eadcc
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-01 09:01:56 +0530

    Unsolved

---
+++ 03_WeeklyContest291/02_KDividibleSubArray.cpp
@@ -0,0 +1,27 @@
+// 6049. K Divisible Elements Subarrays
+
+// Given an integer array nums and two integers k and p, 
+// return the number of distinct subarrays which have at most k 
+// elements divisible by p.
+
+// Two arrays nums1 and nums2 are said to be distinct if:
+
+// They are of different lengths, or
+// There exists at least one index i where nums1[i] != nums2[i].
+// A subarray is defined as a non-empty contiguous sequence of 
+// elements in an array.
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int countDistinct(vector<int>& nums, int k, int p) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
