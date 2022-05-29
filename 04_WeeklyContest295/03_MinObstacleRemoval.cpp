3deb43adf9a318ff3bfa737a207ea2dd2c42056e
shubhansu-kr <shubhansu2021@gmail.com>
2022-05-29 09:29:08 +0530

    Unsolved

---
+++ 04_WeeklyContest295/03_MinObstacleRemoval.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/contest/weekly-contest-295/problems/minimum-obstacle-removal-to-reach-corner/
+
+// You are given a 0-indexed 2D integer array grid of size m x n. Each 
+// cell has one of two values:
+
+// 0 represents an empty cell,
+// 1 represents an obstacle that may be removed.
+// You can move up, down, left, or right from and to an empty cell.
+
+// Return the minimum number of obstacles to remove so you can move 
+// from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1).
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int minimumObstacles(vector<vector<int>>& grid) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
