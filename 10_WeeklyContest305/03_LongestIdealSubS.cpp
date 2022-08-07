58f4db3186ab5118e737622849af6b9380009e8b
shubhansu-kr <shubhansu2021@gmail.com>
2022-08-07 08:17:39 +0530

    Unsolved

---
+++ 10_WeeklyContest305/03_LongestIdealSubS.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/contest/weekly-contest-305/problems/longest-ideal-subsequence/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+class Solution {
+public:
+    int longestIdealString(string s, int k) {
+        
+    }
+};
+
+int main () {
+    
+    return 0;
+}
